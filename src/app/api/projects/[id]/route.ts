082b01ca1ae11c4fb0c83fc04586fd4df414e4ec
Ysaac Andujar Binet <ysaac@codika.net>
2025-11-10 22:23:16 -0400

    patch projects

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -1,6 +1,8 @@
+import { ProjectStatus } from "@/db/schema";
 import { db, projects } from "@/lib/db";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 
 export async function DELETE(_: NextRequest, context: { params: Promise<{ id: string }> }) {
   const { id } = await context.params;
@@ -12,3 +14,28 @@ export async function DELETE(_: NextRequest, context: { params: Promise<{ id: st
     }
 }
 
+export async function PATCH(req: NextRequest, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
+
+  const schema = z.object({
+    status: z.enum(ProjectStatus.enumValues, { message: "Seleccione un estado válido" }),
+  });
+
+  try {
+    const body = await req.json();
+    const parsed = schema.parse(body);
+
+    await db
+      .update(projects)
+      .set({ status: parsed.status })
+      .where(eq(projects.id, Number(id)));
+
+    return new NextResponse(null, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const message = error.issues?.[0]?.message ?? "Datos inválidos";
+      return NextResponse.json({ error: message }, { status: 400 });
+    }
+    return NextResponse.json({error: 'Error eliminando el proyecto'}, { status: 400 });
+  }
+}
