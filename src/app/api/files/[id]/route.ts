39302d7a44f4a9ea6da7a9ab83db4dd286672727
Ysaac Andujar Binet <ysaac@codika.net>
2025-11-12 20:20:19 -0400

    delte file on filelist

--- src/app/api/files/[id]/route.ts
+++ src/app/api/files/[id]/route.ts
@@ -1,11 +1,12 @@
-import { eq } from 'drizzle-orm';
 import { files } from "@/db/schema";
 import { db } from "@/lib/db";
 import { s3 } from "@/lib/s3";
-import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { eq } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(_: Request, context: { params: Promise<{ id: string }> }) {
+export async function GET(_: NextRequest, context: { params: Promise<{ id: string }> }) {
   const { id } = await context.params;
   if (!id)
     return Response.json({ error: "Missing key" }, { status: 400 });
@@ -24,4 +25,35 @@ export async function GET(_: Request, context: { params: Promise<{ id: string }>
   const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
 
   return Response.json({ url: signedUrl, name: file?.name });
+}
+
+
+export async function DELETE(_: NextRequest, context: { params: { id: string } }) {
+  const { id } = await context.params;
+  try {
+    const file = await db
+      .select()
+      .from(files)
+      .where(eq(files.id, Number(id)))
+      .limit(1)
+      .then((r) => r[0]);
+
+    if (!file) {
+      return NextResponse.json({ error: "Archivo no encontrado" }, { status: 404 });
+    }
+    await s3.send(
+      new DeleteObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME!,
+        Key: file.key,
+      })
+    );
+    await db.delete(files).where(eq(files.id, Number(id)));
+    return new NextResponse(null, { status: 200 });
+  } catch (error) {
+    console.error("Error eliminando archivo:", error);
+    return NextResponse.json(
+      { error: "Error eliminando el archivo" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/files/components/FilesListItem.tsx
+++ src/app/files/components/FilesListItem.tsx
@@ -1,3 +1,4 @@
+import { DeleteModal } from "@/components/DeleteModal";
 import { Button } from "@/components/ui/button";
 import { useDownloadFile } from "@/hooks/files";
 import { Files } from "@/lib/db";
@@ -6,47 +7,40 @@ import { es } from "date-fns/locale";
 import { Download, Trash2 } from "lucide-react";
 
 export const FilesListItem = ({ file }: { file: Files }) => {
-  const { mutate: downloadFile, isPending } = useDownloadFile()
-  return (
+    const { mutate: downloadFile, isPending } = useDownloadFile()
+    return (
         <div
-          key={file.id}
-          className="flex flex-col md:flex-row md:items-center justify-between p-4 border rounded-lg bg-background shadow-sm"
+            key={file.id}
+            className="flex flex-col md:flex-row md:items-center justify-between p-4 border rounded-lg bg-background shadow-sm"
         >
-          <div className="flex flex-col">
-            <span className="font-semibold text-foreground">{file.name}</span>
-            {file.description && (
-              <span className="text-sm text-muted-foreground">
-                {file.description}
-              </span>
-            )}
-            <div className="flex gap-3 text-sm text-muted-foreground mt-1">
-              <span>{file.fileType}</span>
-              <span>•</span>
-              <span>
-                {format(new Date(file.createdAt), "dd MMM yyyy, HH:mm", { locale: es })}
-              </span>
+            <div className="flex flex-col">
+                <span className="font-semibold text-foreground">{file.name}</span>
+                {file.description && (
+                    <span className="text-sm text-muted-foreground">
+                        {file.description}
+                    </span>
+                )}
+                <div className="flex gap-3 text-sm text-muted-foreground mt-1">
+                    <span>{file.fileType}</span>
+                    <span>•</span>
+                    <span>
+                        {format(new Date(file.createdAt), "dd MMM yyyy, HH:mm", { locale: es })}
+                    </span>
+                </div>
             </div>
-          </div>
 
-          <div className="flex items-center gap-2 mt-3 md:mt-0">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => downloadFile(file.key)}
-              disabled={isPending}
-              className="text-blue-600 hover:text-blue-700 cursor-pointer"
-            >
-              <Download className="w-4 h-4 mr-1" /> {isPending ? "Descargando..." : "Descargar"}
-            </Button>
-            <Button
-              variant="ghost"
-              size="sm"
-              className="text-red-500 hover:text-red-600 cursor-pointer"
-              onClick={() => console.log("Eliminar", file.name)}
-            >
-              <Trash2 className="w-4 h-4" />
-            </Button>
-          </div>
+            <div className="flex items-center gap-2 mt-3 md:mt-0">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => downloadFile(file.key)}
+                    disabled={isPending}
+                    className="text-blue-600 hover:text-blue-700 cursor-pointer"
+                >
+                    <Download className="w-4 h-4 mr-1" /> {isPending ? "Descargando..." : "Descargar"}
+                </Button>
+                <DeleteModal queryKey={["files"]} url={`api/files/${file.id}`} />
+            </div>
         </div>
-  )
+    )
 }
